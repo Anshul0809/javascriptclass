a3e6696ca33edc066207553ad95dc54a1dd62cc8
Anshul0809 <anshulgajbhiye23@gmail.com>
2025-05-02 21:15:47 +0530

    new

--- day1.js
+++ day1.js
@@ -1,14 +1,14 @@
 // external javascript
 'use strict'//2015=>ES6(ubdate)
-var num1=34
-var num2=23
-console.log(num1+num2)
-console.log(num1-num2)
-console.log(num1*num2)
-console.log(num1/num2)
-console.log(num1%num2)
+// var num1=34
+// var num2=23
+// console.log(num1+num2)
+// console.log(num1-num2)
+// console.log(num1*num2)
+// console.log(num1/num2)
+// console.log(num1%num2)
 
 
-var a=10
-b=24
-console.log(b)
+// var a=10
+// b=24
+// console.log(b)
---
+++ day2.js
@@ -0,0 +1,38 @@
+
+function subh(){
+    // console.log("welcome tomy first function")
+    // var a=18
+    // var b=5
+    // console.log( 'sum of '+a+ '+'+b+'=' +(a+b))
+
+    //bom(browser object model)
+
+    // window.alert('this is alert message')
+//    var ans= window.confirm('are you okay')
+//    console.log(ans) 
+
+   var name= window.prompt('Do you know my name')
+   console.log(name) 
+
+}
+function patel(){
+    console.log("patel calling")
+}
+// patel()
+// subh()
+function a(){
+    var a=window.prompt('enter 1st number')
+
+}
+function b(){
+    var b=window.prompt('enter second number')
+}
+
+function c(){
+    
+   window.alert.log(a+b)
+}
+a()
+b()
+c()
+
