b662819b18b42af359a4dd81e794761297405bc1
Anshul0809 <anshulgajbhiye23@gmail.com>
2025-05-10 19:41:51 +0530

    new

--- DOM.js
+++ DOM.js
@@ -1,11 +1,21 @@
 // <!-- Dom=document object model
 //     DOM is like a tree structure html ka  (jisme js aage kuchh bhi change kar sakti hai,(dynamicaly run kar sakti hai) ) -->
 // method=EventListener (attribute ke sath lagta hai (onclick="Anshul()"))
-function Anshul(){
+// function Anshul(){
  
 //  alert("okay")
 // console.log()
-document.write("okay")
-}
+// document.write("okay")
+// }
 
 // Anshul()
+
+// function first() {
+//     alert("Anshul")
+// }
+// function second(){
+//     alert("gajbhiye")
+// }
+// function third(){
+//     alert("boy")
+//  }
---
+++ string.js
@@ -0,0 +1,31 @@
+// string 
+// it is a primitive data type
+// it is used to represent the text 
+// ## method
+// 1.length
+// 2.slice()
+// 3.concat()
+// 4.substring()
+// 5.replace() *
+// 6.touppercase()
+// 7.tolowercase()
+// 8.trim()
+// 9.replaceAll()*
+// 10.charAt()
+// 11.charcodeAt()
+// 12.split() *
+// 13.indexOf()
+
+// let text="  sid israni  " //white space
+
+function len(){
+    let text="siddarth" //string
+console.log(text.length) //length of the string
+}
+len() //output 8
+
+// 2.slice()
+ let text="this isstringfrom cybrom"
+ console.log(text.slice(7,13)) //output this is string
+
+ 
