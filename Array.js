33b30b9150f4a5d177390387d133643b91cd823e
Anshul0809 <anshulgajbhiye23@gmail.com>
2025-05-13 19:32:13 +0530

    new

---
+++ Array.js
@@ -0,0 +1,16 @@
+//def. Array is collection of heterogeneous element
+//example = var a=[10,20,"appple","banana"]
+// let a=new Array(1,2,3,4,5)
+let a=new Array(1,2,3,4,5)
+console.log(a) 
+// Array methods
+// 1.length
+// 2.pop()  (Remove the last element)
+// 3.push() (Add the element at the end)
+// 4.unshift() (Add the element at the beginning/starting)
+// 5.shift()   (remove the frist element)
+// 6.splice() (Remove the element at the specific index)
+// 7.slice()  (Copy the element at the specific index)
+
+
+
---
+++ template_letrals.js
@@ -0,0 +1,24 @@
+//Template Letrals (` `) it supports single quotes(') and double("")
+// it support multiline strings
+// it support HTML tags
+// 
+function cybrom(){
+alert(`we are from "cybrom" which is in 'bhopal'`)
+}
+let okk=`hello p52 batch from cybrom
+we are from "cybrom" which is in 'bhopal`
+console.log(okk)
+
+let str=`<h1> hello world </h1>`
+document.write(str)
+
+// this method is importent for template literals
+ 
+let Name="Anshul"
+let age=20
+// alert("I am "+Name +"And i am"+ age)
+alert(`I am ${Name} and i am ${age} years Old`)
+ 
+let a=5
+let b=7
+alert(`the sum of ${a} and ${b} is ${a+b}`)
