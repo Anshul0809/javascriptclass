d4ee4c3b732986307cbd8cd377f9a9cb31c0f20d
Anshul0809 <anshulgajbhiye23@gmail.com>
2025-05-14 19:33:28 +0530

    new

--- Array.js
+++ Array.js
@@ -11,6 +11,23 @@ console.log(a)
 // 5.shift()   (remove the frist element)
 // 6.splice() (Remove the element at the specific index)
 // 7.slice()  (Copy the element at the specific index)
+// 8.join()   (conver array to string)
+// 8.toString()   (conver array to string)
 
 
+let fruits=["apple","banana","mango"]
+// fruits.pop()
+// fruits.shift()
+// fruits.push("grapes")
+// fruits.unshift("orange","grapes")
+// console.log(fruits.length)
+// console.log(fruits[1]) //array value access
+console.log(fruits.toString())  // prints the array as a string
+console.log(typeof (fruits.toString()))  
+// let ans=fruits.toString()
+// console.log(typeof(ans))
+console.log(fruits.join('-'))
+console.log(typeof(fruits.join())) 
 
+
+console.log(fruits)
