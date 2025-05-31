2dad122f4252baf12e249b24e9c7ec16129254f4
Anshul0809 <anshulgajbhiye23@gmail.com>
2025-05-31 22:02:51 +0530

    new

--- form.js
+++ form.js
@@ -11,12 +11,13 @@ let check=()=>{
     let errpass=document.querySelector("#errpass")
     let errcpass=document.querySelector("#errcpass")
 
-    if(name.value==""){
+    if(name.value.trim()==""){
         errname.innerHTML="Please enter your name"
         return false
     }
     else if(num.value==""){
         errnum.innerHTML="Please enter your number"
+        errname.innerHTML=""
         return false
 
     }
@@ -30,21 +31,43 @@ let check=()=>{
     }
     else if( !(email.value=="" || email.value.includes("@") && email.value.includes(".com")) ){
         erremail.innerHTML="Please enter valid email"
+        // errname.innerHTML=""
+        // errnum.innerHTML=""
         return false
     }
     else if (pass.value==""){
         errpass.innerHTML="Please enter your password"
+        errname.innerHTML=""
+        errnum.innerHTML=""
+        erremail.innerHTML=""
         return false
     }
     else if (pass.value.length<8){
         errpass.innerHTML="Please enter password of min. 8 character"
+        
         return false
     }
-    else if (pass.value!=cpass.value){
-        errcpass.innerHTML="Please enter same password"
-        return false
-    }
-
-
     
+    else if(!(pass.value.match(/[1234567890]/)
+            && pass.value.match(/[!@#$%^&*()_=+-]/)
+            && pass.value.match(/[A-Z]/)
+            && pass.value.match(/[a-z]/))) //regular expression
+{
+    errpass.innerHTML="Please enter password with at least one uppercase letter, one lowercase letter, on number and one special character"
+    return false
 }
+else if (pass.value!=cpass.value){
+        errcpass.innerHTML="Please enter same password"
+        // errname.innerHTML=""
+        // errnum.innerHTML=""
+        // erremail.innerHTML=""
+        // errpass.innerHTML=""
+        cpass.value=""
+        cpass.focus()
+        
+        return false
+    }
+
+   
+}
+
